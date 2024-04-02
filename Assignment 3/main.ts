ec359afa01ee293a1bbb84d1f6cbd0115f38963e
FariIdrees <talatjabeen1178@gmail.com>
2024-04-03 00:50:44 +0500

    first commit

---
+++ Assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "Nothing is impossible because impossible itself I M POSSIBLE"
+
+let author:string = "Fari Idrees";
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
